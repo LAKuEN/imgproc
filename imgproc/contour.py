f74b807c6ab42855943cc85280b8680a2ed62c65
LAKuEN <y.s.k.brave@gmail.com>
2019-08-18 09:14:41 +0900

    :feat 輪郭の階層構造の一部を取り出すための関数を実装
    同階層の要素をすべて取り出す関数や先頭位置を特定する関数などを実装した

---
+++ imgproc/contour.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""輪郭情報を処理する関数モジュール.
+
+* 輪郭情報
+* 階層情報
+"""
+import numpy as np
+
+
+NEXT_INDEX, PREV_INDEX, FIRST_CHILD_INDEX, PARENT_INDEX = range(4)
+
+
+def get_first_item_index(hierarchy: [np.ndarray]) -> int:
+    """最上位の輪郭のindexを取得.
+
+    cv2.RETR_TREEで取得した輪郭情報を入力として得る前提
+    最も上位層の先頭の輪郭のindexを検索して返す
+    """
+    # FIXME 関数名をより適したものに直す
+    for i, h in enumerate(hierarchy[0]):
+        if h[PREV_INDEX] == -1 and h[PARENT_INDEX] == -1:
+            return i
+
+
+def get_first_item_index_in_same_depth(hierarchy: [np.ndarray],
+                                       idx: int) -> int:
+    """同じ階層の内、先頭の要素のindexを返却."""
+    prev_idx = hierarchy[0][idx][PREV_INDEX]
+    if prev_idx != -1:
+        return get_first_item_index_in_same_depth(hierarchy, prev_idx)
+
+    return idx
+
+
+def get_item_indices_which_connnected_after(hierarchy: [np.ndarray],
+                                            idx: int,
+                                            idxes: [int] = None) -> int:
+    """指定した要素に連なる要素を全て取得."""
+    idxes = [] if idxes is None else idxes
+    idxes.append(idx)
+    next_idx = hierarchy[0][idx][NEXT_INDEX]
+    if next_idx != -1:
+        idxes = get_item_indices_which_connnected_after(hierarchy,
+                                                        next_idx, idxes)
+
+    return idxes
+
+
+def get_item_indices_in_same_depth(hierarchy: [np.ndarray], idx: int) -> [int]:
+    """同じ階層の要素のindexを全件取得し返却."""
+    first_idx = get_first_item_index_in_same_depth(hierarchy, idx)
+    idxes = get_item_indices_which_connnected_after(hierarchy, first_idx)
+
+    return idxes
---
+++ tests/test_contour.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""contour.pyのunittest."""
+import pytest
+
+import numpy as np
+
+from imgproc import contour
+
+
+class TestGetFirstItemIndex:
+    """get_first_item_index() のテストケース."""
+
+    hierarchy_a = np.array([[
+        [1, -1, -1, -1],
+        [2, 0, -1, -1],
+        [3, 1, -1, -1],
+        [4, 2, -1, -1],
+        [-1, 3, -1, -1],
+    ]])
+
+    hierarchy_b = np.array([[
+        [1, -1, -1, 4],
+        [2, 0, -1, 4],
+        [3, 1, -1, 4],
+        [-1, 2, -1, 4],
+        [-1, -1, 0, -1],
+    ]])
+
+    test_data = [
+        (hierarchy_a, 0),
+        (hierarchy_b, 4),
+    ]
+
+    @pytest.mark.parametrize("hierarchy, want", test_data)
+    def test_normal(self, hierarchy, want):
+        """正常系."""
+        got = contour.get_first_item_index(hierarchy)
+        assert want == got
+
+
+class TestGetFirstItemIndexInSameDepth:
+    """get_first_item_index_in_same_depth() のテストケース."""
+
+    hierarchy_a = np.array([[
+        [1, -1, -1, -1],
+        [2, 0, -1, -1],
+        [3, 1, -1, -1],
+        [4, 2, -1, -1],
+        [-1, 3, -1, -1],
+    ]])
+
+    hierarchy_b = np.array([[
+        [2, 1, -1, -1],
+        [0, -1, -1, -1],
+        [3, 0, -1, -1],
+        [4, 2, -1, -1],
+        [-1, 3, -1, -1],
+    ]])
+
+    test_data = [
+        (hierarchy_a, 0, 0),
+        (hierarchy_a, 2, 0),
+        (hierarchy_a, 4, 0),
+        (hierarchy_b, 0, 1),
+        (hierarchy_b, 1, 1),
+    ]
+
+    @pytest.mark.parametrize("hierarchy, idx, want", test_data)
+    def test_normal(self, hierarchy, idx, want):
+        """正常系."""
+        got = contour.get_first_item_index_in_same_depth(hierarchy, idx)
+        assert want == got
+
+
+class TestGetItemIndicesInSameDepth:
+    """get_item_idices_in_same_depth() のテストケース."""
+
+    hierarchy = np.array([[
+        [1, -1, -1, -1],
+        [2, 0, -1, -1],
+        [3, 1, -1, -1],
+        [4, 2, -1, -1],
+        [-1, 3, -1, -1],
+    ]])
+
+    test_data = [
+        (hierarchy, 0, [0, 1, 2, 3, 4]),
+        (hierarchy, 2, [0, 1, 2, 3, 4]),
+        (hierarchy, 4, [0, 1, 2, 3, 4]),
+    ]
+
+    @pytest.mark.parametrize("hierarchy, idx, want", test_data)
+    def test_normal(self, hierarchy, idx, want):
+        """正常系."""
+        got = contour.get_item_indices_in_same_depth(hierarchy, idx)
+        assert want == got
